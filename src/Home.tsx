c6fa5ae4d472212bf5e3d6e7f7a3f37bf4d9d424
onuraladi <ceonurce@hotmail.com>
2022-02-11 15:52:55 +0300

    test

--- src/Home.tsx
+++ src/Home.tsx
@@ -506,7 +506,7 @@ const Home = (props: HomeProps) => {
                 <MintContainer>
                     <DesContainer>
                         <NFT elevation={3}>
-                            <h2>SBC Mint Page</h2>
+                            <h2>SBC Mint Page -v2</h2>
                             <br/>
                             <div><Price
                                 label={isActive && whitelistEnabled && (whitelistTokenBalance > 0) ? (whitelistPrice + " " + priceLabel) : (price + " " + priceLabel)}/><Image
